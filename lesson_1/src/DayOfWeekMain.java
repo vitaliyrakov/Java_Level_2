be58bc233ce3f6db1ed3fc5639e4e8f8fca37d42
vitaliyR <74015913+vitaliyrakov@users.noreply.github.com>
2021-01-14 14:05:25 +0300

    Update DayOfWeekMain.java

--- lesson_1/src/DayOfWeekMain.java
+++ lesson_1/src/DayOfWeekMain.java
@@ -7,13 +7,14 @@ public class DayOfWeekMain {
     }
 
     private static int getWorkingHours(DayOfWeek day) {
-        if (day.equals(DayOfWeek.SATURDAY) || day.equals(DayOfWeek.SUNDAY))
+       if (day.equals(DayOfWeek.SATURDAY) || day.equals(DayOfWeek.SUNDAY))
             System.out.println("Сегодня выходной");
 
+        int workDay=0;
         for (DayOfWeek d : DayOfWeek.values()) {
-            if (day.ordinal() >= d.ordinal() && d!= DayOfWeek.SUNDAY && d!=DayOfWeek.SATURDAY)
-                System.out.println(d);
+            if ((d.equals(day) || workDay>0) && d!= DayOfWeek.SUNDAY && d!=DayOfWeek.SATURDAY)
+                workDay++;
         }
-        return 1;
+        return workDay*WORKING_HOURS_PER_DAY;
     }
 }
