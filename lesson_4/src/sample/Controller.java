cd1efa3acc75801872de9c1060824ff03513d9bc
vitaliyR <74015913+vitaliyrakov@users.noreply.github.com>
2021-01-28 17:21:46 +0300

    Update Controller.java

--- lesson_4/src/sample/Controller.java
+++ lesson_4/src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField textField;
+
+    @FXML
+    public void clickSend(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
