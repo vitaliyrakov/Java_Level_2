69ef165c8e21df6b8ddc48f80494565a234e3d5b
vitaliyR <74015913+vitaliyrakov@users.noreply.github.com>
2021-01-28 17:21:11 +0300

    Update Main.java

--- lesson_4/src/sample/Main.java
+++ lesson_4/src/sample/Main.java
@@ -9,13 +9,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Чат");
+        primaryStage.setScene(new Scene(root, 250, 350));
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
